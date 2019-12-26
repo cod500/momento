e37dcf5bbb9cae2a80d70e195eea3503eaf11a7e
cod5000 <cod5000@gmail.com>
2019-12-26 10:02:37 -0500

    Remote local host

--- public/admin/js/callback-request.js
+++ public/admin/js/callback-request.js
@@ -1,5 +1,5 @@
 const getCallbackRequests = async () =>{
-    return await fetch('http://localhost:4000/callback')
+    return await fetch('/callback')
     .then((response) => response.json())
     .then((data) => {
         return data
@@ -11,7 +11,7 @@ let requestsBlock = document.querySelector('#v-pills-callback');
 requestsBlock.addEventListener('click', (e) =>{
     if(e.target.classList.contains('btn-remove')){
         let id = e.target.parentNode.parentNode.querySelector('.id').value;
-        fetch('http://localhost:4000/callback/' + id, {
+        fetch('/callback/' + id, {
             method: 'DELETE'
         }).then((response) =>{
             response.text().then(() =>{
